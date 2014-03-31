17027bec52c0cee1ab9f0642338aae25d32f1734
Cesar Crusius <ccrusius@cisco.com>
2014-03-31 11:27:12 -0700

    Bunch of vimium commands for chrome

--- chrome.py
+++ chrome.py
@@ -1,25 +1,34 @@
-from dragonfly import (Grammar, AppContext, MappingRule, Integer, Key)
+from dragonfly import (Grammar, AppContext, MappingRule, Integer, Key, Text)
 
 class GlobalChromeMappings(MappingRule):
     mapping = {
-        'close tab': Key('c-w'),
-	'open new tab': Key('c-t'),
-        'reopen tab': Key('cs-t'),
-        'go to next tab': Key('c-tab'),
-        'go to previous tab': Key('cs-tab'),
+        'close tab': Key('x'),                           # vimium
+	'open new tab': Key('t'),                        # vimium
+        'duplicate tab': Key('y/25,t'),                  # vimium
+        'reopen tab': Key('X'),                          # vimium
+        '(go to) next tab': Key('K'),                    # vimium
+        '(go to) previous tab': Key('J'),                # vimium
         'go to tab <tab>': Key('c-%(tab)d'),
-        'go to last tab': Key('c-9'),
-        'go back': Key('a-left'),
-        'go forward': Key('a-right'),
-        'reload page': Key('c-r'),
+        '(go to) first tab': Key('g,0'),                 # vimium
+        '(go to) last tab': Key('g,dollar'),             # vimium
+        'go back': Key('H'),                             # vimium
+        'go forward': Key('L'),                          # vimium
+        'go to address': Key('a-d'),
+        'reload page': Key('r'),                         # vimium
+        'show labels': Key('f'),                         # vimium
+        'show labels in new tab': Key('escape/25, s-f'), # vimium
+        '(go to) label <number>': Text('%(number)d'),    # vimium
+        'duplicate tab': Key('y,t'),                     # vimium
         }
     extras=[
-        Integer('tab', 1, 8)
+        Integer('tab', 1, 8),
+        Integer('number', 1, 9999)
     ]
 
 context = AppContext(executable='chrome')
 grammar=Grammar('Google Chrome',context=context)
 grammar.add_rule(GlobalChromeMappings())
+grammar.load()
 
 def unload():
     global grammar
